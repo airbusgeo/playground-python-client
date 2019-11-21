89b3d0d234624b25b193c34cacf8f5757de248e6
Jean-Francois (Jeff) Faudi <jean-francois.faudi@airbus.com>
2019-11-21 17:19:07 -0500

    Added functions to manage imagery

--- playgroundclient.py
+++ playgroundclient.py
@@ -1,8 +1,32 @@
 import json
 import requests
 import time
+import numpy as np
+from io import BytesIO
+#from multiprocessing import Pool
+from queue import Queue
+from threading import Thread, Lock
+import mercantile
+import math
+from PIL import Image
 import os.path
 
+
+IMG_MODE = 'RGB'
+IMG_SIDE = 256
+
+class NetException(Exception):
+    """Tile Exception: generic error for Internet"""
+    def __init__(self, message):
+        """Tile Exception contructor"""
+        super(NetException, self).__init__(message)
+
+class TileException(Exception):
+    """Tile Exception: generic error for Tile"""
+    def __init__(self, status_code, message):
+        """Tile Exception contructor"""
+        super(TileException, self).__init__(message)
+        self.status_code = status_code
 
 class PlaygroundClientError(Exception):
     """Exception raised for errors in the Playground Client.
@@ -92,40 +116,40 @@ class PlaygroundClient(object):
     def _get_request(self, template, **kwargs):
         self.playground_refresh_access_token()
         r = requests.get(template.format(**kwargs), headers=self.HEADERS)
-        if r.status_code == 404:
+        if r.status_code == 200:
+            return r.json()
+        elif r.status_code == 404:
             raise PlaygroundClientError(r.status_code, 'This object does not exist')
         elif r.status_code == 401 or r.status_code == 403:
             raise PlaygroundClientError(r.status_code, 'You do not have sufficient rights to perform this operation')
-        elif r.status_code != 200:
-            raise PlaygroundClientError(r.status_code, 'A problem occured during connection with the Playground')
         else:
-            return r.json()
+            raise PlaygroundClientError(r.status_code, 'A problem occured during connection with the Playground')
         return None
 
     def _put_request(self, template, payload, **kwargs):
         self.playground_refresh_access_token()
         r = requests.put(template.format(**kwargs), data=payload, headers=self.HEADERS)
-        if r.status_code == 404:
+        if r.status_code == 200:
+            return r.json()
+        elif r.status_code == 404:
             raise PlaygroundClientError(r.status_code, 'This object does not exist')
         elif r.status_code == 401 or r.status_code == 403:
             raise PlaygroundClientError(r.status_code, 'You do not have sufficient rights to perform this operation')
-        elif r.status_code != 200:
-            raise PlaygroundClientError(r.status_code, 'A problem occured during connection with the Playground')
         else:
-            return r.json()
+            raise PlaygroundClientError(r.status_code, 'A problem occured during connection with the Playground')
         return None
 
     def _delete_request(self, template, **kwargs):
         self.playground_refresh_access_token()
         r = requests.delete(template.format(**kwargs), headers=self.HEADERS)
-        if r.status_code == 404:
+        if r.status_code == 200:
+            return True
+        elif r.status_code == 404:
             raise PlaygroundClientError(r.status_code, 'This object does not exist')
         elif r.status_code == 401 or r.status_code == 403:
             raise PlaygroundClientError(r.status_code, 'You do not have sufficient rights to perform this operation')
-        elif r.status_code != 200:
-            raise PlaygroundClientError(r.status_code, 'A problem occured during connection with the Playground')
         else:
-            return True
+            raise PlaygroundClientError(r.status_code, 'A problem occured during connection with the Playground')
         return False
     
     PLAYGROUND_CURRENT_USER_URL = PLAYGROUND_URL + "/currentUser"
@@ -189,3 +213,108 @@ class PlaygroundClient(object):
     PLAYGROUND_SEARCH_SOURCEID_URL = PLAYGROUND_URL + "/api/catalog/sourceid/{sourceId}"
     def get_image_from_sourceid(self, sourceId):
         return self._get_request(self.PLAYGROUND_SEARCH_SOURCEID_URL, sourceId=sourceId)
+
+    # function to get a single XYZ tile
+    def get_tile(self, xyz_url, z, x, y):
+        self.playground_refresh_access_token()
+        r = requests.get(xyz_url.format(z=z, x=x, y=y), headers=self.HEADERS)
+        if r.status_code == 200:
+            return Image.open(BytesIO(r.content))
+        elif r.status_code == 404 or r.status_code == 204:
+            return Image.new(IMG_MODE, (IMG_SIDE, IMG_SIDE), "black")
+        elif r.status_code == 401 or r.status_code == 403:
+            raise PlaygroundClientError(r.status_code, 'You do not have sufficient rights to perform this operation')
+        else:
+            raise PlaygroundClientError(r.status_code, 'A problem occured during connection with the Playground')
+        return None    
+    
+    # multithreaded functions to get multiple XYZ tiles and combine them into a large tile
+    def get_big_tile(self, nbtiles, xyz_url, zoom, col, row):
+        big_tile = Image.new(IMG_MODE, (IMG_SIDE * nbtiles, IMG_SIDE * nbtiles))
+        offset = nbtiles // 2
+
+        # Get Lock
+        queueLock = Lock()
+
+        def patchwork(q):
+            while True:
+                args = q.get()
+                if args is None:
+                    #print("Received None -> exiting")
+                    break
+                (j, i) = args
+                #print("Processing {}/{}".format(j, i))
+                c = col - offset + i
+                r = row - offset + j
+                try:
+                    img = self.get_tile(xyz_url, zoom, c, r)
+                except (TileException, NetException) as te:
+                    print("In except clause: putting {}/{} back in queue".format(j, i))
+                    q.put(args)
+                    continue
+                finally:
+                    q.task_done()
+
+                # paste in big image making sure that it is thread-safe
+                queueLock.acquire()
+                big_tile.paste(img, (i * IMG_SIDE, j * IMG_SIDE))
+                queueLock.release()
+
+        matrix = [[(j, i) for j in range(nbtiles)] for i in range(nbtiles)]
+        patches = [item for sublist in matrix for item in sublist]
+        #print(patches)
+
+        q = Queue(100000)
+        for p in patches:
+            q.put(p)
+
+        threads = []
+        num_worker_threads = 8
+        for _ in range(num_worker_threads):
+            t = Thread(target=patchwork, args=(q,))
+            t.start()
+            threads.append(t)
+
+        # block until all tasks are done
+        q.join()
+
+        # stop workers
+        for i in range(num_worker_threads):
+            q.put(None)
+        for t in threads:
+            t.join()
+
+        return big_tile
+    
+    # creates the corresponding PROJ file to the previous image
+    def get_big_tile_proj(self, nbtiles, zoom, col, row):
+
+        col_min = col - nbtiles // 2
+        row_min = row - nbtiles // 2
+
+        (easting, northing) = num2merc(col_min, row_min, zoom)
+        content = str(2.0 * maxvalue / (2.0 ** zoom) / 256.0) + "\n"
+        content += "0.0\n"
+        content += "0.0\n"
+        content += str(-2.0 * maxvalue / (2.0 ** zoom) / 256.0) + "\n"
+        content += str(easting) + "\n"
+        content += str(northing) + "\n"
+        return content
+
+
+    
+# some useful function to convert to WebMercator
+# maxvalue = 2 * math.pi * 6378137 / 2.0
+maxvalue = 20037508.342789244
+
+def merc2num(easting, northing, zoom):
+    n = 2.0 ** zoom
+    xtile = int((easting / maxvalue + 1.0) / 2.0 * n)
+    ytile = int((1.0 - northing / maxvalue) / 2.0 * n)
+    return (xtile, ytile)
+
+def num2merc(xtile, ytile, zoom):
+    n = 2.0 ** zoom
+    easting = (2.0 * xtile / n - 1.0) * maxvalue
+    northing = (1.0 - 2.0 * ytile / n) * maxvalue
+    return (easting, northing)
